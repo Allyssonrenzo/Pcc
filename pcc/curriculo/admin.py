aba4d644144c20b2ea4565e8d27713244579f1ac
Allyssonrenzo <130418629+Allyssonrenzo@users.noreply.github.com>
2024-08-20 15:14:07 -0300

    Delete pcc directory

--- pcc/curriculo/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Curriculo
-
-admin.site.register(Curriculo)
--- pcc/usuario/context_processors.py
+++
@@ -1,6 +0,0 @@
-def admin_AIOA(request):
-    # Sua lógica para calcular o contexto
-    context = {
-        'example_key': 'example_value',
-    }
-    return context
